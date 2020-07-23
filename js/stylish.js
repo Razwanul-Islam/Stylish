7b808dafe9d80c8c2bceff7ff0eb8374d9ae5d15
Razwanul Islam <razwanrakib0@gmail.com>
2020-07-23 10:04:14 +0600

    Update Navbar

--- js/stylish.js
+++ js/stylish.js
@@ -1,14 +1,22 @@
 var isCollapsed = true;
 $(document).ready(function () {
   //get default color of nav to collapse menu
-  $('.collapse').css({"background":$('.s-row').parent().css('background')})
+  
   $(".toggle").click(function () {
+    $('.collapsed').css({"background":$('.s-row').parent().css('background')})
+    var t=$(".nav-row-2").offset().top;
+    var h=$(".nav-row-2").css('height');
+    h=Number(h.slice(0,h.length-2));
+  
+    $(".collapsed").css({"top":String(Math.floor(t+h))+"px"})
+      
     if (isCollapsed) {
-      $(".collapse").animate({ left: "0px" });
+      $(".collapsed").animate({ left: "0px" });
       isCollapsed = false;
       $(".toggle").text("<--");
     } else {
-      $(".collapse").animate({ left: "-250px" });
+      
+      $(".collapsed").animate({ left: "-250px" });
       isCollapsed = true;
       $(".toggle").text("-->");
     }
